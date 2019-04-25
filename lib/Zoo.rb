b308917d40c69277beba91f6037d849adb4199a1
mmcgovern0 <mmcgovern.wework@gmail.com>
2019-04-25 18:06:20 -0400

    Update Animal and Zoo class

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,49 @@
 class Zoo
+	
+	attr_reader :name, :location
+
+	@@all = []
+
+	def initialize (name, location)
+		@name = name
+		@location = location
+		@@all << self 
+	end
+
+	def self.all
+		@@all
+	end
+
+	def animals
+		Animal.all.select do |animal|
+			animal.zoo == self
+		end
+	end
+
+	def animal_species
+		species_array = self.animals.map do |animal|
+			animal.species
+		end
+		species_array.uniq
+	end
+
+	def find_by_species(species)
+		Animal.all.select do |animal|
+			animal.zoo == self
+		end
+	end
+
+	def animal_nicknames
+		nickname_array = self.animals.map do |animal|
+			animal.nickname
+		end
+		nickname_array.uniq
+	end
+
+	def self.find_by_location(location)
+		self.all.select do |zoo|
+			zoo.location == location
+		end
+	end	
 
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,20 @@ require 'pry'
 
 
 #Test your code here
+animal_1 = Animal.new("nick","cat",100)
+animal_2 = Animal.new("doug","dog",200)
+animal_3 = Animal.new("james","fish",400)
+animal_4 = Animal.new("ryan","bird",500)
 
+
+zoo_a = Zoo.new("a","NY")
+zoo_b = Zoo.new("b","BK")
+zoo_c = Zoo.new("c","BX")
+
+animal_1.zoo = zoo_a
+animal_2.zoo = zoo_b
+animal_3.zoo = zoo_c
+animal_4.zoo = zoo_c
 
 binding.pry
 puts "done"
