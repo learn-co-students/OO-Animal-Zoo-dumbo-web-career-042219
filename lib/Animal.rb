e268858b6518afe7ae4185009eeea78d6dba0f16
askoenig <askoenig94@gmail.com>
2019-04-25 17:21:42 -0400

    Finished Zoo

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,29 @@
 class Animal
 
+  attr_reader :species, :nickname, :zoo
+  attr_accessor :weight
+
+  @@all = []
+
+
+  def initialize(species, weight, nickname, zoo)
+    @species = species
+    @weight = weight
+    @nickname = nickname
+    @@all << self
+    @zoo = zoo
+    # self == instance of Animal
+  end
+
+  def self.all
+    @@all
+    # self == Animal class
+  end
+
+  def self.find_by_species(species)
+    species_array = @@all.select do |animal|
+      animal.species == species
+    end
+  end
+
 end
