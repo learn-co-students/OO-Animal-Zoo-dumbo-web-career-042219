8674ccb4079a1f0fb06eee70311fe5d311ac5c37
github username <github email address>
2019-04-25 17:03:19 -0400

    Done.

--- run.rb
+++ run.rb
@@ -2,9 +2,16 @@ require_relative "lib/Animal.rb"
 require_relative "lib/Zoo.rb"
 require 'pry'
 
+bx_zoo = Zoo.new("BX Zoo", "Bronx")
+cpk_zoo = Zoo.new("CPK Zoo", "Manhattan")
+big_tiger = Animal.new("tiger", "800lbs", "tigger")
+small_monkey = Animal.new(
+  "monkey", "30lbs", "curious george")
+  huge_elephant = Animal.new("elephant", "12,000lbs","dumbo")
+big_tiger.zoo = cpk_zoo
+small_monkey.zoo = bx_zoo
+huge_elephant.zoo = bx_zoo
 
 #Test your code here
-
-
 binding.pry
 puts "done"
