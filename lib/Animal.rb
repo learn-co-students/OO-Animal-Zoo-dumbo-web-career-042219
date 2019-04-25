ead8ed064ab786ff0da85f76b9c8d3aa5f9f8a74
sebastosh <sebastosh@gmail.com>
2019-04-25 17:55:05 -0400

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,34 @@
 class Animal
 
+attr_reader :species, :nickname, :zoo
+attr_accessor :weight
+
+@@all = []
+
+	def initialize(species, weight, nickname, zoo)
+	@species = species
+	@nickname = nickname
+	@weight = weight
+	@zoo = zoo
+	@@all << self
+	end
+
+
+	def self.all
+	@@all
+	end
+
+	def zoo
+	@zoo
+	end
+
+	# def species
+	# @species
+	# end
+
+	def self.find_by_species(argument)
+		Animal.all.select do |animal|
+		animal.species == argument
+	end
+end
 end
--- run.rb
+++ run.rb
@@ -2,9 +2,13 @@ require_relative "lib/Animal.rb"
 require_relative "lib/Zoo.rb"
 require 'pry'
 
+bronx = Zoo.new("Bronx, NYC", "Bronx Zoo")
+san_diego = Zoo.new("San Diego, CA", "San Diego Zoo")
+jardin = Zoo.new("Paris, FR" , "Jardins Zoologiques")
 
-#Test your code here
-
+polar_bear = bronx.new_animal("Ursus", "600lbs" , "Goodnight")
+grizzly = bronx.new_animal("Ursus", "500lbs" , "Go2sleep")
+jacques = jardin.new_animal("Pelicanus", "24lbs", "FishEater")
 
 binding.pry
 puts "done"
